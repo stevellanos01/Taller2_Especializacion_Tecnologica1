2ddef561f52117abc071f7abe25901da1c8ace2a
estebanllanos01 <johndoe@example.com>
2022-06-17 18:08:42 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <dashboard />
+  </div>
+</template>
+
+<script>
+import dashboard from './components/Dashboard.vue';
+
+export default {
+  name: "App",
+  components: {
+    dashboard,
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
