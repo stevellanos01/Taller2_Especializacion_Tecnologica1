4b74fe11fb4a3049fcda642b47c8ab2cd07f9d8b
estebanllanos01 <johndoe@example.com>
2022-06-17 18:17:20 -0400

    se agrega style a App.vue

--- src/App.vue
+++ src/App.vue
@@ -16,5 +16,25 @@ export default {
 </script>
 
 <style scoped>
-
+@import url('https://fonts.googleapis.com/css2?family=Black+Ops+One&display=swap');
+* {
+  
+  margin: 0;
+  padding: 0;
+}
+.app {
+  font-family: 'Black Ops One', cursive;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+  margin-top: 10px;
+}
+::-webkit-scrollbar {
+  width: 3px;
+  background: black;
+}
+::-webkit-scrollbar-thumb {
+  background-color: black;
+}
 </style>
