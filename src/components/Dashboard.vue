4854337cab5d0c9913c6676809c2421287028cb6
estebanllanos01 <johndoe@example.com>
2022-06-17 20:49:16 -0400

    se crea matriz inicial de tablero

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,9 +1,50 @@
 <template>
-
+  <div>
+    <div class="container">
+      <h1>GATO GAME</h1>
+      <h2 class="estado">
+        <span  class="resultado">
+          Resultado:
+        </span>
+        <span > Turno:</span>
+      </h2>
+      <div v-for="(row, n) in tablero" :key="n" class="row">
+        <div v-for="(box, m) in row" :key="m">
+          <cell/>
+        </div>
+      </div>
+      <button>
+        <h3>Limpiar</h3>
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
+import cell from "./Cell.vue";
 
+export default {
+  name: "juego",
+  data() {
+    // datos iniciales
+    return {
+      tablero: [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""],
+      ],
+      current: true,
+      win: "",
+      score: { O: 0, X: 0 },
+    };
+  },
+  methods: {
+
+  },
+  components: {
+    cell
+  },
+};
 </script>
 
 <style scoped>
