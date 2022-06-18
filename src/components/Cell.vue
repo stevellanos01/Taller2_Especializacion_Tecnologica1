576896b0694cf9091efe8ec9d708f7066349e5fd
estebanllanos01 <johndoe@example.com>
2022-06-17 20:27:14 -0400

    se agrega funcion y estilo a celda

--- src/components/Cell.vue
+++ src/components/Cell.vue
@@ -1,11 +1,35 @@
 <template>
-
+  <div v-on:click="clicked">
+    {{ valor }}
+  </div>
 </template>
 
 <script>
-
+export default {
+  name: "celda",
+  props: ["valor", "posicionX", "posicionY"],
+  methods: {
+    clicked() {
+      this.$emit("hit", [this.posicionX, this.posicionY]);
+    },
+  },
+};
 </script>
 
 <style scoped>
-
+div {
+  padding: 5px;
+  height: 100px;
+  width: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 5px solid black;
+  font-size: 80px;
+  cursor: pointer;
+  animation: border-color 0.5s ease-in-out;
+}
+div:active {
+  border-color: red;
+}
 </style>
